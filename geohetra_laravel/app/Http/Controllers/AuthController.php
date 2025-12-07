1032db01489d4adbc8b95c9aae12b98f1be279b2
Mahens3 <razakatiambolaphillipe@gmail.com>
2025-12-07 10:47:00 +0300

    Optimisation cote react et laravel

--- geohetra_laravel/app/Http/Controllers/AuthController.php
+++ geohetra_laravel/app/Http/Controllers/AuthController.php
@@ -8,46 +8,47 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
-class AuthController extends Controller {
-    public function login( Request $request ) {
-        $user = User::where( 'phone', $request->phone )->first();
-        if ( $user ) {
-            $user = User::where( 'mdp', $request->mdp )
-            ->where( 'phone', $request->phone )
-            ->first();
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required',
+            'mdp' => 'required'
+        ]);
 
-            if ( $user ) {
-                $credentials = $request->validate( [
-                    'phone' => 'required',
-                    'mdp' => 'required',
-                ] );
+        $user = User::where('phone', $request->phone)->first();
 
-                $token = JWTAuth::fromUser( $user );
-                return response()->json( [ 
-                    'token' => $token,
-                    'phone' => true,
-                    'mdp' => true,
-                ], 200 );
-            } else {
-                return [
-                    'phone' => true,
-                    'mdp' => false
-                ];
-            }
-        } else {
-            return [
+        if (!$user) {
+            return response()->json([
                 'phone' => false,
                 'mdp' => false
-            ];
+            ]);
         }
 
+        if ($user->mdp !== $request->mdp) {
+            return response()->json([
+                'phone' => true,
+                'mdp' => false
+            ]);
+        }
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'phone' => true,
+            'mdp' => true,
+            'token' => $token
+        ], 200);
     }
 
-    protected function respondWithToken( $token ) {
-        return response()->json( [
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-        ] );
+        ]);
     }
 }
